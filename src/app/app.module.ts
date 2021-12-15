7575f01527c26e5d9f46a13046c97478aab99585
MariaLuizaDMoura <malumoura10@gmail.com>
2021-12-15 08:42:21 -0300

    IMPLEMENTAÇÃO DO PUT E DELETE DE POSTAGENS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { TemaComponent } from './tema/tema.component';
 
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 
 
 @NgModule({
@@ -27,7 +29,9 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
     InicioComponent,
     TemaComponent,
     TemaDeleteComponent,
-    TemaEditComponent
+    TemaEditComponent,
+    PostagemDeleteComponent,
+    PostagemEditComponent
   ],
   imports: [
     BrowserModule,
--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -15,12 +15,23 @@ export class PostagemService {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
-  getAllPostagens(): Observable<Postagem[]>{
+  getAllPostagens(): Observable<Postagem[]> {
     return this.http.get<Postagem[]>('https://olajovemblog.herokuapp.com/postagens', this.token)
   }
 
-  postPostagem(postagem: Postagem) : Observable<Postagem>{
+  getByIdPostagem(id: number): Observable<Postagem> {
+    return this.http.get<Postagem>(`https://olajovemblog.herokuapp.com/postagens/${id}`, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem> {
+    return this.http.put<Postagem>('https://olajovemblog.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  postPostagem(postagem: Postagem): Observable<Postagem> {
     return this.http.post<Postagem>('https://olajovemblog.herokuapp.com/postagens', postagem, this.token)
   }
 
+  deletePostagem(id: number){
+    return this.http.delete(`https://olajovemblog.herokuapp.com/postagens/${id}`, this.token)
+  }
 }
